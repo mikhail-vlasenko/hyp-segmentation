0c1594f1f20cee91e90d494a0ae2c7130f615e86
Mikhail <mikhail.v.vlasenko@gmail.com>
2025-05-17 20:39:48 +0200

    adapt transform embeds for the part-first hierarchy

--- hierarchy_embeddings/transform_embeds.py
+++ hierarchy_embeddings/transform_embeds.py
@@ -1,3 +1,4 @@
+import json
 import sys
 sys.path.append("..")
 
@@ -7,12 +8,20 @@ import torch
 BG_CLASS_INDEX = 0
 ROOT_CLASS_INDEX = 204
 # root_dir = "../h_embeds/"
-root_dir = "/home/misha/projects/hyp-segmentation/hierarchy_embeddings/hierarchies/hierarchy_embeddings/experiments/spin_dataset/spin_hierarchy/"
-embed_dir = root_dir + "2025-04-14_204854/"
-level = 1
+root_dir = "/home/misha/projects/hyp-segmentation/hierarchy_embeddings/hierarchies/hierarchy_embeddings/experiments/spin_dataset/spin_hierarchy"
+
+is_part_first = True
+if is_part_first:
+    root_dir += "_part-first"
+    ROOT_CLASS_INDEX = 1
+embed_dir = root_dir + "/2025-05-17_195428/"
+level = 2
 renormalize = False
-centered_bg = True  # when true, the bg is set to root, making it near the center of the disk
-dim = 64
+centered_bg = False  # when true, the bg is set to root, making it near the center of the disk
+
+with open(embed_dir + "config.json", "r") as f:
+    config = json.load(f)
+    dim = config["embedding_dim"]
 
 file_path = embed_dir + f"HierarchyEmbedding_weights_{dim}.pth"
 save_path = embed_dir + (f"HierarchyEmbedding_weights_{dim}"
@@ -20,7 +29,7 @@ save_path = embed_dir + (f"HierarchyEmbedding_weights_{dim}"
                          f"_only_level_{level}"
                          f"{'_renormalized' if renormalize else ''}.pth")
 
-embeddings = torch.load(file_path)
+embeddings = torch.load(file_path, weights_only=False)
 
 if centered_bg:
     bg = embeddings.embeddings.weight.tensor[ROOT_CLASS_INDEX]
@@ -29,19 +38,27 @@ else:
     bg = embeddings.embeddings.weight.tensor[0]
 
 if level == 3:
+    if is_part_first:
+        raise ValueError("level 3 is not supported for part-first")
     embeddings.embeddings.weight.tensor = torch.cat(
         (bg.unsqueeze(0), embeddings.embeddings.weight.tensor[1:ROOT_CLASS_INDEX]), dim=0
     )
 
 # level2 is 0 for bg and 216 to 255 (incl)
 if level == 2:
-    embeddings.embeddings.weight.tensor = embeddings.embeddings.weight.tensor[216:256]
+    if is_part_first:
+        embeddings.embeddings.weight.tensor = embeddings.embeddings.weight.tensor[16:56]
+    else:
+        embeddings.embeddings.weight.tensor = embeddings.embeddings.weight.tensor[216:256]
     embeddings.embeddings.weight.tensor = torch.cat(
         (embeddings.embeddings.weight.tensor, bg.unsqueeze(0)), dim=0
     )
 
 if level == 1:
-    embeddings.embeddings.weight.tensor = embeddings.embeddings.weight.tensor[205:216]
+    if is_part_first:
+        embeddings.embeddings.weight.tensor = embeddings.embeddings.weight.tensor[2:16]
+    else:
+        embeddings.embeddings.weight.tensor = embeddings.embeddings.weight.tensor[205:216]
     embeddings.embeddings.weight.tensor = torch.cat(
         (embeddings.embeddings.weight.tensor, bg.unsqueeze(0)), dim=0
     )
